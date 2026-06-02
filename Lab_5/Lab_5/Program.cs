6e47e2694d22467441ecff21885d8a8ee49d9161
taomka <m.a.kovalenko@student.khai.edu>
2026-06-02 18:04:46 +0300

    fix a few namespaces in files

--- Lab_5/Lab_5/Program.cs
+++ Lab_5/Lab_5/Program.cs
@@ -1,63 +1,65 @@
-﻿using Lab_5;
-using System.Text;
+﻿using System.Text;
 
-class Program
+namespace Lab_5
 {
-    static void Main()
+    class Program
     {
-        Console.OutputEncoding = Encoding.UTF8;
-        while (true)
+        static void Main()
         {
-            Console.WriteLine("Транспортна задача");
-            Console.WriteLine("1. Запустити на прикладі");
-            Console.WriteLine("2. Ввести дані вручну");
-            Console.WriteLine("ESC. Вийти");
-            switch (Console.ReadKey())
+            Console.OutputEncoding = Encoding.UTF8;
+            while (true)
             {
-                case var key when key.Key == ConsoleKey.D1:
-                    RunPresetData();
-                    break;
-                case var key when key.Key == ConsoleKey.D2:
-                    RunManualInput();
-                    break;
-                case var key when key.Key == ConsoleKey.Escape:
-                    return;
+                Console.WriteLine("Транспортна задача");
+                Console.WriteLine("1. Запустити на прикладі");
+                Console.WriteLine("2. Ввести дані вручну");
+                Console.WriteLine("ESC. Вийти");
+                switch (Console.ReadKey())
+                {
+                    case var key when key.Key == ConsoleKey.D1:
+                        RunPresetData();
+                        break;
+                    case var key when key.Key == ConsoleKey.D2:
+                        RunManualInput();
+                        break;
+                    case var key when key.Key == ConsoleKey.Escape:
+                        return;
+                }
             }
         }
-    }
 
-    static void RunPresetData()
-    {
-        double[,] C = {
+        static void RunPresetData()
+        {
+            double[,] C = {
                 { 9, 8, 6, 9 },
                 { 4, 7, 5, 10 },
                 { 10, 8, 6, 8 }
             };
-        double[] A = [70, 65, 55];
-        double[] B = [85, 35, 30, 40];
+            double[] A = [70, 65, 55];
+            double[] B = [85, 35, 30, 40];
 
-        TransportSolver.Solve(C, A, B);
-    }
-
-    static void RunManualInput()
-    {
-        int m = ConsoleHelpers.ReadInt("Введіть кількість пунктів відправлення (m): ");
-        int n = ConsoleHelpers.ReadInt("Введіть кількість пунктів призначення (n): ");
-
-        double[,] C = new double[m, n];
-        Console.WriteLine("\nВведіть матрицю вартостей (по рядках, через пробіл):");
-        for (int i = 0; i < m; i++)
-        {
-            double[] row = ConsoleHelpers.ParseRow(n);
-            for (int j = 0; j < n; j++) C[i, j] = row[j];
+            TransportSolver.Solve(C, A, B);
         }
 
-        Console.WriteLine("\nВведіть вектор запасів (через пробіл):");
-        double[] A = ConsoleHelpers.ParseRow(m);
+        static void RunManualInput()
+        {
+            int m = ConsoleHelpers.ReadInt("Введіть кількість пунктів відправлення (m): ");
+            int n = ConsoleHelpers.ReadInt("Введіть кількість пунктів призначення (n): ");
 
-        Console.WriteLine("\nВведіть вектор заявок (через пробіл):");
-        double[] B = ConsoleHelpers.ParseRow(n);
+            double[,] C = new double[m, n];
+            Console.WriteLine("\nВведіть матрицю вартостей (по рядках, через пробіл):");
+            for (int i = 0; i < m; i++)
+            {
+                double[] row = ConsoleHelpers.ParseRow(n);
+                for (int j = 0; j < n; j++) C[i, j] = row[j];
+            }
 
-        TransportSolver.Solve(C, A, B);
+            Console.WriteLine("\nВведіть вектор запасів (через пробіл):");
+            double[] A = ConsoleHelpers.ParseRow(m);
+
+            Console.WriteLine("\nВведіть вектор заявок (через пробіл):");
+            double[] B = ConsoleHelpers.ParseRow(n);
+
+            TransportSolver.Solve(C, A, B);
+        }
     }
 }
